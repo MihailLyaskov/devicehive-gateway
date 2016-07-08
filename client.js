4d8ffc03c0a3d4d4a018cddbd71903a82cb3ac92
mlyaskov <m.lyaskov@abv.bg>
2016-07-08 15:04:42 -0700

    add test suport for commands

--- client.js
+++ client.js
@@ -1,19 +1,24 @@
 global.XMLHttpRequest = require('xhr2');
 global.WebSocket = require('ws');
 var DeviceHive = require('./devicehive.client');
+var Commands = require('./commands');
 
 var Client = function (options){
 	var DH = new DeviceHive(options.apiurl , options.accessKey);
-
+	var commands = new Commands(DH);
+	
+	this.commands=commands;
 	this.DH = DH;
-	this.command = "gpio/write";
-	this.parameters1 = { "5":1 };
-	this.parameters0 = { "5":0 };
+
 };
 
 Client.prototype.start = function (cb,handler){
 	var self = this;
-
+	var data = {
+		device : "esp-device",
+		command : "gpio/write" ,
+		parameters : { "5":1 } 
+	}
 	
 
 	console.log("trying to open chanell!");
@@ -22,16 +27,20 @@ Client.prototype.start = function (cb,handler){
 	                console.log(err);
 	            }
 	            console.log("trying to send command!");
-				self.DH.sendCommand('esp-device',self.command, self.parameters1 ,function(err, res) {
+				self.commands.SendCommand(data);
+
+
+
+				/*self.DH.sendCommand('esp-device',self.command, self.parameters1 ,function(err, res) {
 					if(err){
 						console.log(err);
 					}
 					console.log("equipment sendNotification " + "esp-device" + " " + self.command);
 					console.log(res);
 					return;
-				});
+				}); */
 
-				setTimeout( function(){
+				/*setTimeout( function(){
 					self.DH.sendCommand('esp-device',self.command, self.parameters0 ,function(err, res) {
 						if(err){
 							console.log(err);
@@ -49,7 +58,7 @@ Client.prototype.start = function (cb,handler){
 						}
 					);
 
-				},3000);
+				},3000);*/
 	        }, 
 	        "websocket"
 	);
